ff6b5b441c749541956ecd24fcd450bcf346be4b
weilyuwang <weilyuwang@gmail.com>
2020-09-27 19:19:10 -0400

    :bug: anagram

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -9,3 +9,29 @@ class LinkedList:
     def __init__(self):
         super().__init__()
         self.head = None
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
+    def append(self, data):
+        new_node = Node(data)
+
+        # First check if the current list is empty
+        if self.head is None:
+            self.head = new_node
+            return
+
+        # If no empty, traverse to the very end of the list and append the new item
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+
+llist = LinkedList()
+llist.append('A')
+llist.append('B')
+llist.print_list()
