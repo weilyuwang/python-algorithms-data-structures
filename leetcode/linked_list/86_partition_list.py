7984741191d7b04fd144d7ffc2416a7f78be8c8b
weilyuwang <weilyuwang@gmail.com>
2020-10-04 21:55:43 -0400

    :fire: partition linked list

---
+++ leetcode/linked_list/86_partition_list.py
@@ -0,0 +1,44 @@
+"""
+86. Partition List
+
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+Example:
+
+Input: head = 1->4->3->2->5->2, x = 3
+Output: 1->2->2->4->3->5
+
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        # before and after are the two pointers used to create two list
+        # before_head and after_head are used to save the heads of the two lists.
+        # All of these are initialized with the dummy nodes created.
+        before = before_head = ListNode(-1)
+        after = after_head = ListNode(-1)
+
+        while head:
+            if head.val < x:
+                before.next = head
+                before = before.next
+            else:
+                after.next = head
+                after = after.next
+
+            head = head.next
+
+        after.next = None
+        before.next = after_head.next
+
+        return before_head.next
