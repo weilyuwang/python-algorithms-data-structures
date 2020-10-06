9ef8bc6062901eb0b4c2fb6382ac94115192b0a4
weilyuwang <weilyuwang@gmail.com>
2020-10-06 19:17:01 -0400

    :sparkles: binary search

---
+++ leetcode/binary_search/704_binary_search.py
@@ -0,0 +1,35 @@
+"""
+704. Binary Search
+
+Given a sorted (in ascending order) integer array nums of n elements and a target value, 
+write a function to search target in nums. 
+If target exists, then return its index, otherwise return -1.
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+
+"""
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
