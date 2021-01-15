af2ccf7efd117e7d380172c909ebef59c39cfaab
weilyuwang <weilyuwang@gmail.com>
2021-01-14 23:49:10 -0500

    :zap: sliding window

---
+++ two_pointers/11_container_with_most_water.py
@@ -0,0 +1,55 @@
+'''
+11. Container With Most Water
+
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+ n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, 
+which, together with the x-axis forms a container, such that the container contains the most water.
+Notice that you may not slant the container.
+
+
+Example 1:
+
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+Example 3:
+
+Input: height = [4,3,2,1,4]
+Output: 16
+Example 4:
+
+Input: height = [1,2,1]
+Output: 2
+'''
+
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxarea = 0
+        l = 0
+        r = len(height) - 1
+        
+        while l < r:
+            area = min(height[l], height[r]) * (r-l)
+            maxarea = max(maxarea, area)
+            # always moves the shorter line inwards, otherwise we won't get a larger area
+            # because the area is limited by the shorter line
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1 
+                
+        return maxarea
+
+s = Solution()
+assert s.maxArea([1,8,6,2,5,4,8,3,7]) == 49
+assert s.maxArea([1,1]) == 1
+assert s.maxArea([1,2,1]) == 2
+assert s.maxArea([1,1]) == 1
