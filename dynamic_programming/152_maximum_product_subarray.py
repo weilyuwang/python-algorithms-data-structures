b6be6fd4f372020bb413e9eb53d08502be9becfa
weilyuwang <weilyuwang@gmail.com>
2021-02-13 22:21:19 -0500

    :fire: dp

---
+++ dynamic_programming/152_maximum_product_subarray.py
@@ -0,0 +1,37 @@
+'''
+152. Maximum Product Subarray
+Medium
+
+Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+Example 1:
+
+Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+'''
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = max_so_far
+        
+        for i in range(1, len(nums)):
+            cur = nums[i]
+            temp_max_so_far = max(cur, max_so_far * cur, min_so_far * cur)
+            min_so_far = min(cur, max_so_far * cur, min_so_far * cur)
+            max_so_far = temp_max_so_far
+            
+            result = max(max_so_far, result)
+        
+        return result
---
+++ dynamic_programming/5_longest_palindromic_substring.py
@@ -0,0 +1,69 @@
+'''
+5. Longest Palindromic Substring
+Medium
+
+Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+Example 3:
+
+Input: s = "a"
+Output: "a"
+Example 4:
+
+Input: s = "ac"
+Output: "a"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consist of only digits and English letters (lower-case and/or upper-case),
+
+'''
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+        '''
+        DP:
+        dp[i][j] = {true if substring Si...Sj is a palindrome, false otherwise}
+        dp[i][j] = dp[i+1][j-1] and Si == Sj
+
+        so dp[0][s[]]
+        '''
+
+
+        res = ""
+        resLen = 0
+        
+        for i in range(len(s)):
+            # odd length
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+                
+            # even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+                
+        return res
+            
